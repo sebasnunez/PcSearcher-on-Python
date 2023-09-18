b9c57603dde73553a117949fe2aa10a652dea6c2
sebasnunez <nunezjuanse@gmail.com>
2023-09-18 16:58:50 -0500

    First Version

---
+++ main.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+import subprocess
+@app.route("/", methods = ["POST"])
+def recorrerIp():
+  if request.method=="POST" and 'recorrer' in request.form:
+    ip = request.form.get("ip")
+    file = open("./historico/ips/"+str(ip)+".txt", "w")
+    for i in range(253):
+      res = subprocess.Popen("ping -n 1 " + str(ip)+"."+str(i+2), shell=True, stdout=subprocess.PIPE)
+      out = res.stdout.read()
+      if str(out)[-6]=="s":
+        file.write(str(ip)+"."+str(i+2)+"\n")
+      print(str(ip)+"."+str(i+2)) 
+    file.close()
+    file2 = open("./historico/ips/"+ip+".txt", "r")
+    ls = []
+    for x in file2:
+      ls.append(x)
+    file2.close()
+    listaD = listar()
+    return render_template("index.html",list = ls, opcion = listaD)
+  else:
+    if request.method=="POST" and 'nombres' in request.form:
+      ipB = request.form.get("ipB")
+      print(str(ipB))
+      file = open("./historico/ips/"+str(ipB)+".txt", "r")
+      file2 = open("./historico/hostnames/"+str(ipB)+".txt", "w")
+      for i in file:
+        ip = i
+        res = subprocess.Popen("CD .\PSTools && PsExec \\\\"+str(ip[:-1])+" hostname", shell=True,stdout=subprocess.PIPE)
+        out = res.stdout.read()
+        final = ""
+        if str(out)[-18]=="r":
+          print("Error de Conexion")
+        else:
+          for j in range(13):
+            final += str(out)[-18+j]
+          file2.write(str(ip[:-1])+": "+final+"\n")
+      file.close()  
+      file2.close()
+      file3 = open("./historico/hostnames/"+str(ipB)+".txt", "r")
+      ls = []
+      for x in file3:
+        ls.append(x)
+      file3.close()
+      listaD = listar()
+      return render_template("index.html",names = ls, opcion = listaD)
+    else:
+      if request.method=="POST" and 'historicoRecorrido' in request.form:
+        des = request.form.get("des")
+        file = open("./historico/ips/"+str(des) + ".txt", "r")
+        ls1 = []
+        for x in file:
+          ls1.append(x)
+        file.close()
+        ls2 = []
+        if os.path.isfile("./historico/hostnames/"+str(des)+".txt"):
+          file = open("./historico/hostnames/"+str(des)+".txt", "r")
+          for x in file:
+            ls2.append(x)
+          file.close()
+        listaD = listar()
+        return render_template("index.html",names = ls2, list = ls1, opcion = listaD)
+      else:
+        if request.method=="POST" and 'botonEvidencia' in request.form:
+          ip = request.form.get("evidencia")
+          res = subprocess.Popen("ping -n 1 "+str(ip), shell=True,stdout=subprocess.PIPE)
+          out = res.stdout.read()
+          if str(out)[-6]=="s":
+            tipo = request.form.get("TiEq")
+            evidencia = open("./evidencia.bat", "w")
+            if str(tipo)=="escritorio":
+              evidencia.write('cd pstools\nPsExec \\\\'+str(ip)+' cmd /c (ipconfig/all ^&  PowerShell "Get-PhysicalDisk | Format-Table -AutoSize" ^& wmic memorychip get capacity, manufacturer ^& wmic product where name="Ivanti Notifications Manager" get name, version ^& wmic product where name="Trend Micro Apex One Security Agent" get name, version ^& cd "c:\Program Files (x86)\LANDesk\LDClient" ^& start /B /MIN vulscan.exe ^& start /B /MIN LDISCN32.exe)')
+            else:
+              evidencia.write('cd pstools\nPsExec \\\\'+str(ip)+' cmd /c (ipconfig/all ^&  PowerShell "Get-PhysicalDisk | Format-Table -AutoSize" ^& wmic memorychip get capacity, manufacturer ^& wmic product where name="Ivanti Notifications Manager" get name, version ^& wmic product where name="Trend Micro Apex One Security Agent" get name, version ^& cd "c:\Program Files (x86)\LANDesk\LDClient" ^& start /B /MIN vulscan.exe ^& start /B /MIN LDISCN32.exe ^& manage-bde -status)')
+            res = subprocess.run("start evidencia.bat", shell=True)
+            listaD = listar()
+            return render_template('index.html', opcion = listaD)
+          else:
+            listaD = listar()
+            return render_template('index.html', opcion = listaD, ip = str(ip))
+        else:
+          listaD = listar()
+          return render_template('index.html', opcion = listaD)
+@app.route('/')
+def index():
+  listaD = listar()
+  return render_template('index.html', opcion = listaD)
+
+def listar():
+  dir = os.listdir("./historico/ips")
+  listaD = []
+  for x in dir:
+    listaD.append(str(x)[:-4])
+  return listaD
+
+if __name__ == "__main__":
+  app.run()
